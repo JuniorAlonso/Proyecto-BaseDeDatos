6cf66c9c4c5a5f17569c759f844108a8be3906eb
JuniorAlonso <alonsocb56@gmail.com>
2026-07-02 09:53:15 -0500

    Subiendo todo

---
+++ src/main/java/com/market/LaEstancia/LaEstanciaApplication.java
@@ -0,0 +1,13 @@
+package com.market.LaEstancia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaEstanciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaEstanciaApplication.class, args);
+	}
+
+}
